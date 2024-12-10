a5f18622134beff3144f538793db17c82a476cf8
tauhidul252 <tauhidulislam252@gmail.com>
2024-12-10 08:32:32 +0600

    input, print, new line

---
+++ week_1/c++_input.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+
+    int A=100,B;
+
+    std :: cin >> B; //user input funtation.
+
+    std :: cout << "hello World" << "  "; //c++ print funcation
+
+    std :: cout << A << std::endl << B; //print two value "A" is predefined and "B" is input from the user
+
+
+
+
+    return 0;
+
+}
